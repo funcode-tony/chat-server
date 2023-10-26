4635138aba175002a68eea4b7712c01366a8f3f8
tony <tony.hsueh@funcode-tech.com>
2023-10-26 18:34:54 +0800

    remember sessionid

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const { InMemorySessionStore } = require('./sessionStore')
+const sessionStore = new InMemorySessionStore();
 const app = express()
 const chatServer = require('http').createServer(app)
 const { v4: uuidv4 } = require('uuid')
+const crypto = require('crypto');
+const { maxHeaderSize } = require('http');
+const randomId = () => crypto.randomBytes(8).toString("hex");
 const io = require('socket.io')(chatServer, {
   cors: {
     origin: "http://localhost:3000  ",
@@ -9,59 +14,86 @@ const io = require('socket.io')(chatServer, {
 })
 
 io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  console.log('前端紀錄的sessionID', sessionID)
+  if (sessionID) {
+    const session = sessionStore.findSession(sessionID)
+    // 有 session 不必再次進行登入
+    if (session) {
+      socket.sessionID = sessionID
+      socket.userID = session.userID
+      socket.username = session.username
+      return next()
+    }
+  }
   // 從前端設定的 username
   const username = socket.handshake.auth.username
   // 前端也有檢查，但後端也有檢查更安全
   if (!username) {
     return next(new Error("invalid username"))
   }
+  socket.sessionID = randomId()
+  socket.userID = randomId()
   socket.username = username
   next()
 })
 
 io.on('connection', (socket) => {
+  console.log('sessionid', socket.sessionID)
+  // 改為儲存一個我們自己設定 session，原先是使用 socket.io 給的 id，但重新整理頁面就會再生成一個新的(這樣每次都得登入)
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
+    username: socket.username,
+    connected: true
+  })
+  
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  })
+
+  socket.join(socket.userID)
+
   const users = [];
-
-  for (let [id, socket] of io.of("/").sockets) {
+  sessionStore.findAllSessions().forEach((session) => {
     users.push({
-      userID: id,
-      username: socket.username
+      userID: session.userID,
+      username: session.username,
+      connected: session.connected,
     })
-  }
-
+  })
   socket.emit("users", users)
 
+  // 通知已經在線上的人，有新的使用者上線
   socket.broadcast.emit("user connected", {
-    userID: socket.id,
+    userID: socket.userID,
     username: socket.username,
+    connected: true
   });
 
+  // 因為現在 join 都不是使用 socket 給的id，所以
   socket.on('private message', ({to, content}) => {
-    socket.to(to).emit("private message", {
+    socket.to(to).to(socket.userID).emit("private message", {
       content,
-      from: socket.id,
+      from: socket.userID,
+      to,
     });
   })
 
-  // socket.on("joinroom", ({ id, name, roomId }) => {
-  //   const index = chatRooms.findIndex(room => room.id === roomId)
-  //   chatRooms[index].userNumber++;
-    
-  //   socket.join(roomId)
 
-  //   console.log(roomId);
-  //   // socket.emit("joinSuccess", {
-  //   //   success: true,
-  //   //   message: `您已加入xx室`,
-  //   // });
-  //   io.to(roomId).emit("joinSuccess", {
-  //     message: `${name} 已加入xx室`,
-  //   });
-   
-  // });
+  socket.on("disconnect", async () => {
+    // 需要確認我們自己創的 userID 是不是不存在 io 中
+    const matchingSockets = await io.in(socket.userID).allSockets()
+    const isDisconnected = matchingSockets.size === 0
+    if (isDisconnected) {
+      socket.broadcast.emit("user disconnect", socket.userID)
 
-  socket.on("disconnect", () => {
-    console.log("離線囉");
+      sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        username: socket.username,
+        connected: false
+      })
+    }
   });
 });
 
